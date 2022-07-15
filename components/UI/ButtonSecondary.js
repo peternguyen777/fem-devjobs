156b8229d85a5b20aecccc5f722d4c8ec143fcac
peternguyen777 <peter.quang.nguyen@gmail.com>
2022-07-15 14:16:35 +1000

    buttons

--- components/UI/ButtonSecondary.js
+++ components/UI/ButtonSecondary.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 const ButtonSecondary = (props) => {
   return (
     <Link href={props.href}>
-      <button className='h-12 rounded-[5px] bg-[#EEEFFC] px-10 hover:bg-[#C4C9F4]'>
+      <button className='h-12 rounded-[5px] bg-[#EEEFFC] px-5 hover:bg-[#C4C9F4]'>
         <h5 className='text-violet'>{props.children}</h5>
       </button>
     </Link>
