2eb7c4d8c14a7aa63e782eab9375c5b3b2767775
peternguyen777 <peter.quang.nguyen@gmail.com>
2022-07-16 02:45:34 +1000

    resolved theme

--- components/UI/ToggleLightDark.js
+++ components/UI/ToggleLightDark.js
@@ -3,46 +3,32 @@ import { useTheme } from "next-themes";
 import { useState, useEffect } from "react";
 
 const ToggleLightDark = () => {
-  const { systemTheme, theme, setTheme } = useTheme();
+  const { systemTheme, theme, resolvedTheme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
     setMounted(true);
   }, []);
 
-  const renderThemeChanger = () => {
-    if (!mounted) return null;
+  if (!mounted) return null;
 
-    const currentTheme = theme === "system" ? systemTheme : theme;
-
-    if (currentTheme === "dark") {
-      return (
-        <span
-          aria-hidden='true'
-          className='inline-block h-[14px] w-[14px] translate-x-[30px] transform rounded-full bg-violet transition duration-100 ease-in-out hover:bg-lightviolet'
-        />
-      );
-    } else {
-      return (
-        <span
-          aria-hidden='true'
-          className='inline-block h-[14px] w-[14px] translate-x-[5px] transform rounded-full bg-violet transition duration-100 ease-in-out hover:bg-lightviolet'
-        />
-      );
-    }
-  };
+  const currentTheme = theme === "system" ? systemTheme : theme;
 
   return (
     <Switch
-      checked={`${theme === "dark" ? true : false}`}
+      checked={`${resolvedTheme === "dark" ? true : false}`}
       onChange={() => {
-        setTheme(theme === "dark" ? "light" : "dark");
+        setTheme(resolvedTheme === "dark" ? "light" : "dark");
       }}
       className='relative inline-flex h-6 w-12 items-center rounded-full bg-white'
     >
       <span className='sr-only'>Enable notifications</span>
-
-      {renderThemeChanger()}
+      <span
+        aria-hidden='true'
+        className={`inline-block h-[14px] w-[14px] ${
+          currentTheme === "dark" ? `translate-x-[30px]` : `translate-x-[5px]`
+        } transform rounded-full bg-violet transition duration-100 ease-in-out hover:bg-lightviolet`}
+      />
     </Switch>
   );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute='class'>
+    <ThemeProvider defaultTheme='system' enableSystem={true} attribute='class'>
       <Component {...pageProps} />
     </ThemeProvider>
   );
