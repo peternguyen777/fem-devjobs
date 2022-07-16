71941d51559dcc040fd1e74fab17e2f6a53bf754
peternguyen777 <peter.quang.nguyen@gmail.com>
2022-07-16 19:09:09 +1000

    secondary button hover state

--- components/UI/ButtonSecondary.js
+++ components/UI/ButtonSecondary.js
@@ -4,8 +4,8 @@ import Link from "next/link";
 const ButtonSecondary = (props) => {
   return (
     <Link href={props.href}>
-      <button className='h-12 rounded-[5px] bg-[#EEEFFC] px-5 hover:bg-[#C4C9F4]'>
-        <h5 className='text-violet'>{props.children}</h5>
+      <button className='h-12 rounded-[5px] bg-[#EEEFFC] px-5 hover:bg-[#C4C9F4] dark:bg-[#303641] dark:hover:bg-[#6a6e76]'>
+        <h5 className='text-violet dark:text-white'>{props.children}</h5>
       </button>
     </Link>
   );
