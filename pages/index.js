fe90e7470bb6f240862b9c7e0c1ae3f45bc3703d
peternguyen777 <peter.quang.nguyen@gmail.com>
2022-07-23 01:12:08 +1000

    sanity update

--- pages/index.js
+++ pages/index.js
@@ -46,7 +46,7 @@ export default function Home({ posts }) {
     setJobData(transformedJobsData);
 
     setIsLoading(false);
-  }, []);
+  }, [posts]);
 
   useEffect(() => {
     fetchJobsHandler();
